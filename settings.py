1b87831ccfc0e2802b583788b73917c7b8ec023e
Ron4Axc1H3g <101228049+Ron4-kw0rk3r@users.noreply.github.com>
2022-04-28 19:16:18 -0300

    Add files via upload
    --up_file

---
+++ settings.py
@@ -0,0 +1,64 @@
+import sys
+import pygame
+
+
+# HOW SETTINGS SIMBOLS
+# FIRTS LINE UPP COMMAND NEW_ PROJECT_LIST_NEW_ METODE
+
+BLOCK = pygame.Rect(20,250,130.5,79.2)
+BLOCK_0 = pygame.Rect(20,335,130.5,79.2)
+BLOCK_1 = pygame.Rect(20,430.1,130.5,79.2)
+BLOCK_2 = pygame.Rect(20,515.1,130.5,79.2)
+BLOCK_3 = pygame.Rect(20,620,130.5,79.2)
+BLOCKC_4 = pygame.Rect(20,708.3,130.5,79.2)
+BLOCK_5 = pygame.Rect(20,795.2,130.5,79.2)
+BLOCK_6 = pygame.Rect(20,870.3,130.5,79.2)
+
+# _blockI = pygame.draw.Rect()
+# _blockJ = pygame.draw.Rect()
+
+
+# HOW _BLOCKS _NUMBERS_
+# 0 1 2 3 4 5 6 7 8 9
+
+A_BLOCK = pygame.Rect(155,250,130.5,79.2)
+B_BLOCK_0 = pygame.Rect(155 ,335, 130.5, 79.2)
+C_BLOCK_1 = pygame.Rect(155 ,430.1, 130.5, 79.2)
+D_BLOCK_2 = pygame.Rect(155 ,515.1, 130.5, 79.2)
+E_BLOCK_3 = pygame.Rect(155 ,620, 130.5, 79.2)
+F_BLOCKC_4 = pygame.Rect(155 ,708.3, 130.5, 79.2)
+G_BLOCK_5 = pygame.Rect(155 ,795.2, 130.5, 79.2)
+H_BLOCK_6 = pygame.Rect(155 ,870.3, 130.5, 79.2)
+# _NumI = pygame.Rect(90 ,65, 50, 50)
+# _NumJ = pygame.Rect(90 ,65, 50, 50)
+
+
+# HOW DIMENCIONS BLOCK _ GRAPH ALL PARAMETERS
+BA_BLOCK = pygame.Rect(289, 250, 130.5 , 79.2 )
+BB_BLOCK_0 = pygame.Rect(289, 335, 130.5, 79.2)
+BC_BLOCK_1 = pygame.Rect(289, 430.1, 130.5, 79.2)
+BD_BLOCK_2 = pygame.Rect(289, 515.1, 130.5, 79.2)
+BE_BLOCK_3 = pygame.Rect(289, 620, 130.5, 79.2)
+BF_BLOCKC_4 = pygame.Rect(289, 708.3, 130.5, 79.2)
+BG_BLOCK_5 = pygame.Rect(289, 795.3, 130.5, 79.2)
+BH_BLOCK_6 = pygame.Rect(289, 870.3, 130.5, 79.2)
+# BG_BLOCK_6 = pygame.Rect(280, 905, 130.5, 79.2)
+# __BLOCK_GRAPH = pygame.Rect(10, 10, 642, 200)
+# __BLOCK_LEFT =
+CA_BLOCK = pygame.Rect( 0x1a7 , 0x313,0x05f, 0x9f)
+CB_BLOCK_0 = pygame.Rect(0x215, 0x2bc, 0x05f, 0x4e)
+CC_BLOCK_1 = pygame.Rect(0x215, 868.3, 0x05f, 0x4e)
+CD_BLOCK_2 = pygame.Rect(0x1a7, 0x2bc, 0x05f, 90.6)
+CE_BLOCK_3 = pygame.Rect(0x1a7, 619.6, 0x05f, 80.6)
+CF_BLOCK_4 = pygame.Rect(0x215, 783, 0x05f, 0x4e)
+CG_BLOCK_5 = pygame.Rect(0x215, 619.6, 0x05f, 0x4e)
+# CH_BLOCK_6 = pygame.Rect(0x19a, 000)
+
+
+
+
+# HOW _MAKE CREATE CHANGE _BUTTOM THEME LIGHT OR DARK
+
+CH_THEME = pygame.Rect(310, 0, 85.6, 69.5)
+# BUILD _NAME_CONFIG_DISPLAY_SETTINGS
+DISPLAY_CHANGE = pygame.Rect(20, 5.2, 700, 100.50)
